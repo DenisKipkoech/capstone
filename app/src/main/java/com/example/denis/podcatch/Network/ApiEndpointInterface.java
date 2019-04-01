733e42df683714cdb65d72785541968e508eeed4
Denis <d3niskip@gmail.com>
2019-04-01 03:20:49 +0300

    Added network calls for curated podcasts and podcast details

--- app/src/main/java/com/example/denis/podcatch/Network/ApiEndpointInterface.java
+++ app/src/main/java/com/example/denis/podcatch/Network/ApiEndpointInterface.java
@@ -1,6 +1,22 @@
 package com.example.denis.podcatch.Network;
 
-public interface ApiEndpointInterface {
+import com.example.denis.podcatch.Models.Podcast;
+import com.example.denis.podcatch.Models.Results;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiEndpointInterface {
+    @GET("curated_podcasts")
+    Call<Results> get_curated_podcasts(
+            @Query("page") int page
+    );
+
+    @GET("podcasts/{id}")
+    Call<Podcast> get_podcast_data(
+            @Path("id") String id
+    );
 
 }
--- app/src/main/java/com/example/denis/podcatch/Network/RetrofitClientInstance.java
+++ app/src/main/java/com/example/denis/podcatch/Network/RetrofitClientInstance.java
@@ -1,13 +1,20 @@
 package com.example.denis.podcatch.Network;
 
+import java.io.IOException;
+
+import okhttp3.Headers;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://listen-api.listennotes.com/api/v2/"        ;
+    private static final String BASE_URL = "https://listen-api.listennotes.com/api/v2/";
+    private static final String auth_token = "1f46e215466b4f128a49f43bacc5d237";
 
     public static Retrofit getRetrofitInstance(){
         if (retrofit == null){
@@ -16,6 +23,21 @@ public class RetrofitClientInstance {
 
             OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
             httpClient.addInterceptor(logging);
+
+            Interceptor headerAuthorisationInterceptor = new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request request = chain.request();
+                    Headers headers = request.headers().newBuilder()
+                            .add("X-ListenApi-Key", auth_token)
+                            .build();
+                    request = request.newBuilder().headers(headers).build();
+
+                    return chain.proceed(request);
+                }
+            };
+
+            httpClient.addInterceptor(headerAuthorisationInterceptor);
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
