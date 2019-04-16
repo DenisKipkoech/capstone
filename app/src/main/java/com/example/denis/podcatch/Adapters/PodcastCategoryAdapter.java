153d035446492b50b9e20556b5a09be5dc7baa7f
Denis <d3niskip@gmail.com>
2019-04-16 07:45:13 +0300

    feat: added click listener to podcast image that opens podcastdetailactivity

--- app/src/main/java/com/example/denis/podcatch/Adapters/PodcastCategoryAdapter.java
+++ app/src/main/java/com/example/denis/podcatch/Adapters/PodcastCategoryAdapter.java
@@ -1,14 +1,17 @@
 package com.example.denis.podcatch.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.denis.podcatch.Models.Podcast;
+import com.example.denis.podcatch.PodcastDetailActivity;
 import com.example.denis.podcatch.R;
 import com.squareup.picasso.Picasso;
 
@@ -38,8 +41,17 @@ public class PodcastCategoryAdapter extends RecyclerView.Adapter<PodcastCategory
                 .load(podcast.getImage())
                 .placeholder(R.mipmap.ic_launcher)
                 .into(holder.podcast_poster);
+        holder.title.setText(podcast.getTitle());
 
+        holder.podcast_poster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, PodcastDetailActivity.class);
+                intent.putExtra("podcast_id", podcast.getId());
 
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -49,9 +61,11 @@ public class PodcastCategoryAdapter extends RecyclerView.Adapter<PodcastCategory
 
     public class CategoryViewHolder extends RecyclerView.ViewHolder{
         ImageView podcast_poster;
+        TextView title;
         public CategoryViewHolder(View itemView) {
             super(itemView);
             podcast_poster = itemView.findViewById(R.id.podcast_pic);
+            title = itemView.findViewById(R.id.podcast_title);
         }
     }
 }
