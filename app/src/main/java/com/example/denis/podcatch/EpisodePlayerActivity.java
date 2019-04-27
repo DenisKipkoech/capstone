54dbea86c1d190bddb15be2b9b0e513946387b25
Denis <d3niskip@gmail.com>
2019-04-27 11:48:54 +0300

    feat: bound activity to audioplayer service

--- app/src/main/java/com/example/denis/podcatch/EpisodePlayerActivity.java
+++ app/src/main/java/com/example/denis/podcatch/EpisodePlayerActivity.java
@@ -1,106 +1,95 @@
 package com.example.denis.podcatch;
 
+
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.denis.podcatch.Models.Constants;
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.example.denis.podcatch.Models.Episode;
+import com.example.denis.podcatch.Service.AudioPlayerService;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.ui.PlayerControlView;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
+import com.squareup.picasso.Picasso;
 
 public class EpisodePlayerActivity extends AppCompatActivity {
     private PlayerControlView playerControlView;
     private SimpleExoPlayer player;
-    boolean playWhenReady;
-    int currentWindow = 0;
-    long playBackPosition = 0;
-    private String URL;
+    private ImageView posterView;
 
-    private static final String TAG = EpisodePlayerActivity.class.getSimpleName();
+    private Episode episode;
+    private boolean mBound;
+    private AudioPlayerService playerService;
 
+
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            AudioPlayerService.LocalBinder localBinder = (AudioPlayerService.LocalBinder) service;
+            playerService = localBinder.getService();
+            mBound = true;
+            initializePlayer();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode_player);
 
-        playerControlView = findViewById(R.id.player_control_view);
-
         Intent intent = getIntent();
-        if (intent != null){
-            URL = intent.getStringExtra(Constants.AUDIO_URL_KEY);
-            Log.d(TAG, "onCreate: Audio Url = "+URL);
+        if (intent != null) {
+            episode = intent.getParcelableExtra(Constants.EPISODE_KEY);
+            setUI(episode);
         }
     }
 
     private void initializePlayer(){
-        player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
-                new DefaultTrackSelector(), new DefaultLoadControl());
-        player.setPlayWhenReady(playWhenReady);
-        player.seekTo(currentWindow, playBackPosition);
-        playerControlView.setPlayer(player);
-
-        if (URL != null){
-            MediaSource mediaSource  = buildMediaSource(Uri.parse(URL));
-            player.prepare(mediaSource, true, false);
-        }
-    }
-
-    private MediaSource buildMediaSource(Uri uri){
-        return new ExtractorMediaSource.Factory(
-                new DefaultHttpDataSourceFactory("PC")
-        ).createMediaSource(uri);
-    }
-
-    private void releasePlayer(){
-        if (player != null){
-            playBackPosition = player.getCurrentPosition();
-            currentWindow = player.getCurrentWindowIndex();
-            playWhenReady = player.getPlayWhenReady();
-            player.release();
-            player = null;
+        if (mBound){
+            player = playerService.getPlayer();
+            playerControlView.setPlayer(player);
         }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (Util.SDK_INT > 23) {
-            initializePlayer();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (Util.SDK_INT > 23 || player == null) {
-            initializePlayer();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (Util.SDK_INT <= 23) {
-            releasePlayer();
-        }
+        Intent serviceintent = new Intent(EpisodePlayerActivity.this,
+                AudioPlayerService.class);
+        serviceintent.putExtra(Constants.EPISODE_KEY, episode);
+        startService(serviceintent);
+        bindService(serviceintent, connection, Context.BIND_AUTO_CREATE);
+        initializePlayer();
+        setUI(episode);
     }
 
     @Override
     protected void onStop() {
+        unbindService(connection);
+        mBound = false;
         super.onStop();
-        if (Util.SDK_INT <= 23) {
-            releasePlayer();
+    }
+
+    private void setUI(Episode episode){
+        posterView = findViewById(R.id.podcast_poster);
+        playerControlView = findViewById(R.id.player_control_view);
+        if (episode.getImage() != null) {
+            Picasso.with(this)
+                    .load(episode.getImage())
+                    .into(posterView);
         }
     }
+
 }
