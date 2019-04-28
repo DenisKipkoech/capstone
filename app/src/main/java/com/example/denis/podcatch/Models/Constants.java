a6cbfb7e4bf137f9eaee4f362fd2dec58bc1442b
Denis <d3niskip@gmail.com>
2019-04-28 22:21:49 +0300

    feat: added episode key

--- app/src/main/java/com/example/denis/podcatch/Models/Constants.java
+++ app/src/main/java/com/example/denis/podcatch/Models/Constants.java
@@ -10,5 +10,5 @@ public class Constants {
     public static final String MY_PREFERENCE = "preference";
     public static final String LIST_STATE_KEY = "liststate";
 
-    public static final String PODCAST_ID_KEY = "podcast_id";
+    public static final String QUERY_KEY = "query_key";
 }
